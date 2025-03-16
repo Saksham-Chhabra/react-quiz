d669946137cec69996750684efe8bca4b92a1e01
Saksham <s.chhabra2005@gmail.com>
2025-03-16 23:28:06 +0530

    react quiz init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useEffect, useReducer } from "react";
+
 import "./App.css";
 import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./main";
+
+const initialState = {
+  questions: [],
+
+  // 'loading' 'error', 'ready', 'active' 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setQuestions":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Invalid action type");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "setQuestions", payload: data }))
+      .catch(dispatch({ type: "dataFailed" }));
+  }, []); // Empty array means this effect will only run once after the first render.
+
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main />
     </div>
   );
 }
